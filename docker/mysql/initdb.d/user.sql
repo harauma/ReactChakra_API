305cabee2e8a56bb73adf87bc54c9f566a043ef7
harauma <fot39hara@gmail.com>
2021-06-29 00:04:14 +0900

    first commit

---
+++ docker/mysql/initdb.d/user.sql
@@ -0,0 +1,9 @@
+USE test_db;
+DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
+  id           INT(10) primary key,
+  name     VARCHAR(40)
+);
+INSERT INTO user (id, name) VALUES (1, "Nagaoka");
+INSERT INTO user (id, name) VALUES (2, "Tanaka");
